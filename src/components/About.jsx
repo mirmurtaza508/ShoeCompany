54e9fc14effe60ce2aab80fb4ccc76325f34051b
mirmurtaza508 <rrahi3473@gmail.com>
2024-08-05 17:21:11 +0530

    update About Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,37 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex h-screen bg-red-500">
-      <div className="w-1/2 bg-green-500">a</div>
-      <div className="w-1/2 bg-gray-500">a</div>
+    <div className="relative flex h-screen flex-wrap border-t-2 bg-black md:grid md:grid-cols-2">
+      <div className="relative mt-20 h-1/2 w-full md:mt-0 md:h-full">
+        <div className="bg-about-backImage h-2/3 brightness-50"></div>
+        <img
+          src="https://s3-alpha-sig.figma.com/img/78d6/2842/51253a82fd5d21364752ab4f158c81f8?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PBAhjF0NuFSPf5vHuC51k42R6KYM9AYot8b2GMJAlKOIMqkwjPlbbvYXTgIZ4-jN6WpH8tvwGxzM~yfoYeGxsEkVYUsicjO0wNX~HhOeXO8vNVCCtHZzegBMsBC930K0R0T0f~JhReFLnTyoq9Q~hLWd-N9OGvMW9i0GINLMd-SJorJHK2kENKu5gzQYzvJhZ89JmPHOji6xJeJQIGeiT4VxfRbWeQMeBTc4lxMMO-u~lLI8TDRDfRWLgelwZZLLChOYIk3pG2~4AAeUsO~r8T~-5r0QK6FdPH1tZ3tTAPoqGul5OqLZ0FXHDn6SP4h~vlNFtE1Qf9WlXyDmQ69Jcw__"
+          alt=""
+          className="absolute top-0 h-96 object-cover md:h-full"
+        />
+      </div>
+      <div className="flex w-full flex-wrap items-center bg-black p-8 text-white md:mt-0">
+        <div className="flex flex-col gap-8">
+          <h1 className="z-16 absolute left-24 top-0 text-5xl font-semibold text-white md:static md:text-left">
+            About Us!
+          </h1>
+          <p className="text-center md:static md:text-start">
+            For each one of those shoe sweethearts out there, THE SHOE COMPANY
+            offer the one-stop goal to pick the correct match of footwear Gone
+            are the days when you needed to go from store to store to locate the
+            <br />
+            <br />
+            correct style and size for yourself At THE SHOE COMPANY, you can
+            locate a vast accumulation of dashing footwear in all sizes and
+            <br /> <br />
+            {/* styles, all inside a couple of snaps. Simple right? Not just we are
+            here to spare your time, we are putting forth you the most dazzling
+            footwear alternatives from all the first-rate brands like DAVINCHI,
+            PRINCESS, and SIGNATURE. At ‘Organization Name’, we offer men,
+            ladies, and child’s shoes perfect for any and each event. */}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,8 @@ export default {
           "url('https://s3-alpha-sig.figma.com/img/87b1/f22d/3c4a82245252ec215de1afd14d9f76c3?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fiQ6ScjEJEGV~iA4v5efsAsLVRrwof6VzrC3kfaxu7AAn0O9bCLESS6cpD0hFXFu75VTcxII8~2phaWErSQrt0iLjf1Yy3Tq7QOCluCMZoIDBxSSKLjvou1xXmHwFWLHssy3Dm6PWonqiDHEuuztPs6E1nsCXg8WYFxceNcWvCI7qIOIk0ML5Y5~jSa997GOCGVEAAc5BgTpTd7ibFS1JWCEWQXmi~F~Ks41vSJ7aILsMbz3yZpB6c3Ob1vVr34qkx94n2Gl4nrTnjsBJzmTKilSXP1VdOIObSjJttotps9a8KPL7mmZoF2GZT6GbK1kaOY0rx4ojEJ7fwvwuj6hzg__')",
         "hero-pattern-2":
           "url('https://s3-alpha-sig.figma.com/img/fb90/8d43/6c32f19d2b5a568106f9b39e91090d3c?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SMPFqtOkdcNqBeFrVvpo8emxNl8JNIUbnPoBukJJU6xx6h6w7Jhz4YTESv-q5z8GmHEpGQtOxrvMdmr9qQexXqcUSGa1SAM206EtoxRBcEvpp75HjkXf11NDzKOiHAHjgFV472mkOu61PhC-B~j4Q0sPnIP87rLRvt~c~~0s22fE5leL02fpVfxRLrR17AHzlBADgN88FS0qDqA0SdwMItBALjqTD8eWRtqhbil1c4UHN0ppo5oD9NkNqZBBzA38EsWwTzoyueTz4ufihVYGo-jCMgvBA907XYkwmndsWfHou3jw2dltvuHEZiyj~tEaqGxnOZ9211PhdwCbT3AMbg__')",
+        "about-backImage":
+          "url('https://s3-alpha-sig.figma.com/img/d84c/0786/528c493b61d58bd198b9df3d0b21d429?Expires=1724025600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hEVJTnMPMq5mZ58q5zyZhE4qiHhxZBdKHS~HiIjCGs~Ox3LCOcc-YCZlR8XtBHMWHrm36NgF9l63HzxiPHDMrhQ2HpgV2gXOMY7Ivha3cs9o5p6Ojx8cgcugPQfDT1zhvwT63ipKqBLu8iu2O6swv3d~HU5DlBh6c5gGAEEEMvXXFTmRV7C3-J0FeRc0Qld748EuNzpXbN7R~peY8OkHNsbdUliv8KoYs1iIV0oTdzvyyyXkNMjLwrDrAB3dGg2piwPt3GxUlQL0AsY42A04gFFf4bJRVp9mHdAHLpun5pYoxfidTwbzozZvPGA4aeLMgK6SMIoSBkhngxSSttk~hg__')",
       },
     },
   },
